374924f46162c3530c2765af9265ba5f8899923b
Zweena Ariva <ariva02zweena@gmail.com>
2024-08-21 09:24:11 +0700

    first commit

---
+++ app/Http/Controllers/JobResponsibiltyController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\JobResponsibilty;
+use Illuminate\Http\Request;
+
+class JobResponsibiltyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(JobResponsibilty $jobResponsibilty)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(JobResponsibilty $jobResponsibilty)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, JobResponsibilty $jobResponsibilty)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(JobResponsibilty $jobResponsibilty)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_08_21_014547_create_job_responsibilties_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use PHPUnit\Framework\Constraint\Constraint;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('job_responsibilties', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('company_job_id')->constrained()->onDelete('cascade');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('job_responsibilties');
+    }
+};
