0c6b932387d8663f482e13171a8bbab07e1bd62c
Zweena Ariva <ariva02zweena@gmail.com>
2024-08-21 09:37:30 +0700

    build with angga

--- app/Models/CompanyJob.php
+++ app/Models/CompanyJob.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CompanyJob extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+    protected $fillable = [
+        'name',
+        'slug',
+        'type',
+        'location',
+        'skill_level',
+        'salary',
+        'thumbnail',
+        'about',
+        'is_open',
+        'company_id',
+        'category_id'
+    ];
 }
--- app/Models/JobResponsibilty.php
+++ app/Models/JobResponsibilty.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class JobResponsibilty extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+    protected $fillable = [
+        'name',
+        'company_job_id'
+    ];
 }
