4797bc745d8cce76222a1ca31c8324eb6d774fdb
Zweena Ariva <ariva02zweena@gmail.com>
2024-08-21 12:12:11 +0700

    bwa

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Access\Gate;
+use Illuminate\Support\Facades\Gate as FacadesGate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+    FacadesGate::before(function($user, $ability){
+        if($user->hasRole('super_admin')){
+                return true;
+            }
+        });
     }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +16,14 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('dashboard')->name('dashboard.')->group(function() {
+        
+    Route::middleware('can:apply job')->group(function(){
+        Route::get('my-applications', [DashboardController::class, 'my_applications'])->name('my.applications');
+        Route::get('my-applications/{job_candidate}', [DashboardController::class, 'my_application_details'])->name('my.applications.details');
+        });
+    });
 });
 
 require __DIR__.'/auth.php';
